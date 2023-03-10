d09d250e71deec02b9cfed75dd164d806715e696
Bekzod1807 <bekzodamanturdiyev@gmail.com>
2023-03-10 16:53:35 +0500

    first commit

---
+++ script/app.js
@@ -0,0 +1,17 @@
+const hour = document.querySelector(".hour");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+
+function setDate() {
+  const now = new Date();
+  const getSecond = now.getSeconds();
+  const getMinute = now.getMinutes();
+  const getHour = now.getHours();
+  const secondDegree = (getSecond / 60) * 360;
+  const minuteDegree = (getMinute * 360) / 60;
+  const hourDegree = (getHour * 360) / 12 + (getMinute * (360 / 60)) / 12;
+  second.style.transform = `rotate(${secondDegree}deg)`;
+  minute.style.transform = `rotate(${minuteDegree}deg)`;
+  hour.style.transform = `rotate(${hourDegree}deg)`;
+}
+setInterval(setDate, 1000);
